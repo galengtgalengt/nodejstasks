084c4d6b5dce6528af44d2364837af863d876c0b
Rahul Gidwani <rahul.gidwani@gmail.com>
2012-02-14 19:59:39 -0800

    Including event into server

--- event.js
+++ event.js
@@ -1,3 +1,7 @@
+this.makeEvent = function(type, description, date, amountRaised) {
+	return new eventInstance(type, description, date, amountRaised);
+}
+
 function eventType(type, description) {
 	this.type = type;
 	this.description = description;
@@ -6,9 +10,11 @@ function eventType(type, description) {
 	};
 };
 
-function eventInstance(type, description, date, amountRaise) {
+function eventInstance(type, description, date, amountRaised) {
+	this.type = type;
+	this.description = description;
 	this.date = date;
 	this.amountRaised = amountRaised;
 }
-eventInstance.prototype = new eventType();
 
+eventInstance.prototype = new eventType();
--- server.js
+++ server.js
@@ -1,27 +1,15 @@
 var http = require('http');
+var foo = require('./event')
+
+var eventOne = foo.makeEvent('foo', 'bar', 'cat', 'dog');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'application/json'});
   res.end(eventOne.toJson());
 }).listen(1337, "127.0.0.1");
+
 console.log('Server running at http://127.0.0.1:1337/');
 console.log(eventOne);
 
 
-function eventType(type, description) {
-	this.type = type;
-	this.description = description;
-	this.toJson = function() {
-		return JSON.stringify(this)
-	};
-};
 
-function eventInstance(type, description, date, amountRaised) {
-	this.type = type;
-	this.description = description;
-	this.date = date;
-	this.amountRaised = amountRaised;
-}
-eventInstance.prototype = new eventType();
-
-var eventOne = new eventInstance('foo', 'bar', 'cat', 'dog');
